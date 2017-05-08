4553e3b2d82e0e76ed4abaaefebb2584dd880857
Tiago Serra <tiago.serra92@gmail.com>
2017-05-08 19:39:51 +0100

    Created part of the CRUD interface for the Posts (namely new and read)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,21 @@
-# This is the post controller, where are implemented the actions of the
-# Controller view
+# This is the post controller, where are implemented the actions of the Posts
+# view
 class PostsController < ApplicationController
   def index
     # code goes here
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:date, :rationale))
+    @post.save
+    redirect_to @post
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 end
