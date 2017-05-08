1a533ec337e5ecfafe9d39d61fc2672ad86efb61
Tiago Serra <tiago.serra92@gmail.com>
2017-05-08 19:10:47 +0100

    Created and tested basic view for the homepage of /posts/

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,7 @@
+# This is the post controller, where are implemented the actions of the
+# Controller view
 class PostsController < ApplicationController
+  def index
+    # code goes here
+  end
 end
