190286950dfbb9ffcedee188cf2f9cb530081dbb
Tiago Serra <tiago.serra92@gmail.com>
2017-05-10 14:08:21 +0100

    Created a helper method to improve UX UI

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,19 @@
+# Module PostsHelper
+# status_label is a method to improve UI/UX
+
 module PostsHelper
+  def status_label(status)
+    status_label_generator(status)
+  end
+
+  def status_label_generator(status)
+    case status
+    when 'submitted'
+      content_tag(:span, status.titleize, class: 'label label-primary')
+    when 'approved'
+      content_tag(:span, status.titleize, class: 'label label-success')
+    when 'rejected'
+      content_tag(:span, status.titleize, class: 'label label-danger')
+    end
+  end
 end
